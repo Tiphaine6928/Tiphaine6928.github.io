01095f498a1703cfe9ad198fe66101ed58444b45
Tiphaine <tiphaine.chapuis69@gmail.com>
2025-02-28 12:16:16 +0100

    Remplissage des expériences passées, projets et changement disposition

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".toggle-btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            const objectives = this.previousElementSibling; // La liste juste avant le bouton
+            objectives.classList.toggle("hidden");
+
+            // Changer le texte du bouton en fonction de l'affichage
+            if (objectives.classList.contains("hidden")) {
+                this.textContent = "Voir plus";
+            } else {
+                this.textContent = "Voir moins";
+            }
+        });
+    });
+});
+
+function toggleDetails(button, className) {
+    const details = button.parentElement.parentElement.querySelector('.' + className);
+    details.style.display = details.style.display === 'none' ? 'block' : 'none';
+}
